bc7cde5d32457ce31d4925dc7502c04485698023
Resh <1138704031@qq.com>
2020-12-07 21:58:12 +0800

    提交代码

---
+++ 两个链表的第一个公共节点.cpp
@@ -0,0 +1,15 @@
+//输入两个链表，找出它们的第一个公共节点。
+//当两个指针相遇时，所指向的结点就是第一个公共结点。
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *node1 = headA;
+        ListNode *node2 = headB;
+        
+        while (node1 != node2) {
+            node1 = node1 != NULL ? node1->next : headB;
+            node2 = node2 != NULL ? node2->next : headA;
+        }
+        return node1;
+    }
+};
