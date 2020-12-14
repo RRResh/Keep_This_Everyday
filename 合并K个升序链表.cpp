2369b81939834d9f5e71752e791fc6678e2c4c7b
Resh <1138704031@qq.com>
2020-12-14 20:05:18 +0800

    提交代码

---
+++ 合并K个升序链表.cpp
@@ -0,0 +1,16 @@
+//给你一个链表数组，每个链表都已经按升序排列。
+//请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ListNode* mergeTwoLists(ListNode *a, ListNode *b) {
+    if ((!a) || (!b)) return a ? a : b;
+    ListNode head, *tail = &head, *aPtr = a, *bPtr = b;
+    while (aPtr && bPtr) {
+        if (aPtr->val < bPtr->val) {
+            tail->next = aPtr; aPtr = aPtr->next;
+        } else {
+            tail->next = bPtr; bPtr = bPtr->next;
+        }
+        tail = tail->next;
+    }
+    tail->next = (aPtr ? aPtr : bPtr);
+    return head.next;
+}
