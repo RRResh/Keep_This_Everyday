71c644c4c3a4d866079b6461bd0f6ed43ac21d61
Resh <1138704031@qq.com>
2021-02-07 23:15:08 +0800

    提交代码

---
+++ 最长公共前缀.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(!strs.size()) return "";
+        string prefix=strs[0];
+        for(int i=1;i<strs.size();i++)
+        {
+            prefix=longestCommonPrefix(prefix,strs[i]);
+            if(!prefix.size()) break;
+        }
+        return prefix;
+    }
+    string longestCommonPrefix(string& str1,string& str2)
+    {
+        int index=0;
+        int length=min(str1.size(),str2.size());
+        while(index<length&&str1[index]==str2[index])
+        {
+            index++;
+        }
+        return str1.substr(0,index);
+    }
+};
